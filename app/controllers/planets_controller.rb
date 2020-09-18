65ea25a92c5c7e15098b82c09ebafad3b03d63ba
MMcClure11 <mcclme02@luther.edu>
2020-09-18 17:22:36 -0500

    adds create method for planet in planet controller

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,4 +5,18 @@ class PlanetsController < ApplicationController
         render json: PlanetSerializer.new(planets).to_serialized_json 
     end 
 
+    def create
+        planet = Planet.new(planet_params)
+        if planet.save
+            render json: PlanetSerializer.new(planet).to_serialized_json 
+        else
+            render json: {message: "Could not save planet"}
+        end
+    end
+
+    private
+
+    def planet_params
+        params.permit(:name, :climate, :planet_image, :env_image, :population)
+    end
 end
